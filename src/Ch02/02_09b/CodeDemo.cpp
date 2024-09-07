120bbf6ba39bde43408ba8496b2147bfd7ad59bd
hopipola-a <64277616+hopipola-a@users.noreply.github.com>
2024-09-07 10:49:54 +0000

    gg

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -4,10 +4,23 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    const size_t LENGTH1 = 25;
+
+    char array_str1[LENGTH1] = "Hey guys! ";
+    char array_str2[] = "What's up ";
+
+    string std_str1 = "Hi everybody! ";
+    string std_str2 = "How's it going. ";
+
+    strncat(array_str1, array_str2, LENGTH1); //adding 
+
+    cout<< array_str1 <<endl;
+    cout<<  std_str1 + std_str2 <<endl; //adding
+
+    cout << endl <<endl;
     return (0);
 }
